3dbe9bd1dd2a8886a8130a86c475eb6286ec2655
Yalou09 <yaellejehu@gmail.com>
2024-01-09 16:15:11 +0100

    j'ai changé la place de l'api

--- index.php
+++ index.php
@@ -111,35 +111,36 @@ session_start();
         </div>
       </div>
     </div>
-    <?php
-    $apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Frotey-lès-Lure&appid=7fdb48df5993f3acec244d1c2652170d";
 
-    $response = file_get_contents($apiUrl);
-
-    if ($response === false) {
-      die('Erreur lors de la requête vers l\'API.');
-    }
-
-    // Convertir  JSON 
-    $data = json_decode($response, true);
-
-    // Afficher les données 
-    echo  $data['name'] . '<br>';
-
-
-    //Convertir en celcius
-    $celcius =  number_format($data['main']['temp'] - 273.15, 0);
-    echo 'Température: ' . $celcius . ' °C<br>';
-    //echo 'Description: ' . $data['weather'][0]['description'] . '<br>';
-
-    // Afficher l'image qui correspond au temps
-    $iconCode = $data['weather'][0]['icon'];
-    $apiIcon = "http://openweathermap.org/img/wn/" . $iconCode . "@2x.png";
-    echo '<img src="' . $apiIcon . '" alt="Image icon temps"><br>';
-    ?>
   </section>
 
+  <!-- API météo -->
+  <?php
+  $apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Frotey-lès-Lure&appid=7fdb48df5993f3acec244d1c2652170d";
 
+  $response = file_get_contents($apiUrl);
+
+  if ($response === false) {
+    die('Erreur lors de la requête vers l\'API.');
+  }
+
+  // Convertir  JSON 
+  $data = json_decode($response, true);
+
+  // Afficher les données 
+  echo  $data['name'] . '<br>';
+
+
+  //Convertir en celcius
+  $celcius =  number_format($data['main']['temp'] - 273.15, 0);
+  echo 'Température: ' . $celcius . ' °C<br>';
+  //echo 'Description: ' . $data['weather'][0]['description'] . '<br>';
+
+  // Afficher l'image qui correspond au temps
+  $iconCode = $data['weather'][0]['icon'];
+  $apiIcon = "http://openweathermap.org/img/wn/" . $iconCode . "@2x.png";
+  echo '<img src="' . $apiIcon . '" alt="Image icon temps"><br>';
+  ?>
 
   <main id="main">
     <section id="about" class="about">
