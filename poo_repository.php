910831dc37e05b1e5d4a81a313f7ed57c543bbf2
Yalou09 <yaellejehu@gmail.com>
2024-01-10 16:33:55 +0100

    jsp

--- poo_repository.php
+++ poo_repository.php
@@ -1,37 +1,43 @@
 <?php
 
-require_once ("poo_models.php");
+require_once("poo_models.php");
 
-class Repository extends Model {
-	
-	function __construct(string $table) { 
+class Repository extends Model
+{
+
+	function __construct(string $table)
+	{
 		parent::__construct($table);
 	}
-	
-	public function requete(string $sql) {
-		
-        //Cas des requêtes simples
-        return $this->db->query($sql);
-    }	
 
-	public function findAll() {
-		$query = $this->requete('SELECT * FROM '.$this->table);
+	public function requete(string $sql)
+	{
+
+		//Cas des requêtes simples
+		return $this->db->query($sql);
+	}
+
+	public function findAll()
+	{
+		$query = $this->requete('SELECT * FROM ' . $this->table);
 		return $query->fetchAll();
 	}
-	
-	public function find(int $id) {
-    // Exécuter la requête
-    return $this->requete("SELECT * FROM {$this->table} WHERE id = $id")->fetch();
+
+	public function find(int $id)
+	{
+		// Exécuter la requête
+		return $this->requete("SELECT * FROM {$this->table} WHERE id = $id")->fetch();
 	}
-	
-	public function findBy(array $criteres) {
+
+	public function findBy(array $criteres)
+	{
 		$champs = [];
 		$valeurs = [];
 
 		//Boucler pour "éclater le tableau"
-		foreach($criteres as $champ => $valeur){
+		foreach ($criteres as $champ => $valeur) {
 			$champs[] = "$champ = ?";
-			$valeurs[]= $valeur;
+			$valeurs[] = $valeur;
 		}
 
 		//Transformer le tableau en chaîne de caractères séparée par des AND
@@ -40,29 +46,31 @@ class Repository extends Model {
 		// Exécuter la requête
 		return $this->requete("SELECT * FROM {$this->table} WHERE $liste_champs", $valeurs)->fetchAll();
 	}
-	
-		//Insertion
-	public function insertData($champs,$valeurs) {
 
-		$sql = "INSERT INTO ".$this->table. '('. $champs.')'." VALUES (".$valeurs.")";
-		
+	//Insertion
+	public function insertData($champs, $valeurs)
+	{
+
+		$sql = "INSERT INTO " . $this->table . '(' . $champs . ')' . " VALUES (" . $valeurs . ")";
+
 		echo $sql;
-		
+
 		$statt = $this->db->prepare($sql);
 
-		$this->db->exec($sql);	
-	
+		$this->db->exec($sql);
 	}
 	//Modification
-	public function updateData($mail,$champ, $valeurs, ){
-        $sql = "UPDATE {$this->table} SET $champ = $valeurs WHERE mail = ?";
-        
-        
-        $stmt = $this->db->prepare($sql);
-        $stmt->execute([$mail]);
-    }
-	
-	public function delete(int $id){
+	public function updateData($mail, $champ, $valeurs,)
+	{
+		$sql = "UPDATE {$this->table} SET $champ = $valeurs WHERE mail = ?";
+
+
+		$stmt = $this->db->prepare($sql);
+		$stmt->execute([$mail]);
+	}
+
+	public function delete(int $id)
+	{
 		return $this->requete("DELETE FROM {$this->table} WHERE id = ?", [$id]);
-}
+	}
 }
